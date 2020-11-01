cc179314676c71db40982919f14bc94c9c0db68d
Andreas Kønigsfeldt <andreaskonigs@live.dk>
2020-11-01 12:14:39 +0100

    Created MVC maps

---
+++ Model/interest.js
@@ -0,0 +1,8 @@
+// Creating class of Interest
+
+class Interest {
+    constructor(hobby1, hobby2, hobby3){
+this.hobby1 = hobby1;
+this.hobby2 = hobby2;
+this.hobby3 = hobby3;
+}}
---
+++ Model/user.js
@@ -0,0 +1,14 @@
+// Creating class of User
+
+class User {
+    constructor(fname, lname, age, gender, location, interest, email, password, image){
+this.fname = fname;
+this.lname = lname;
+this.age = age;
+this.gender = gender;
+this.location = location;
+this.interest = interest;
+this.email = email;
+this.password = password;
+this.image = image;
+}}
